83b38a6d4e9e7514422fbc95ba17b1fd912f8b86
Roberto Cruz Lozano <rcruz15@alumnos.uaq.mx>
2021-05-11 12:01:34 -0500

    Update file

--- umbralizacion.py
+++ umbralizacion.py
@@ -3,14 +3,14 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 img1 = cv2.imread('imagen.extencion',2)
-img3 = np.zeros(img1.shape,dtype=np.int32)
+img2 = np.zeros(img1.shape,dtype=np.int32)
 
 for i in range(img1.shape[0]):
     for j in range(img1.shape[1]):
         if(img1[i][j] <= 127):
-            img3[i][j] = 0
+            img2[i][j] = 0
         if(img1[i][j] > 127):
-            img3[i][j] = 255
+            img2[i][j] = 255
 
 ret,thresh1 = cv2.threshold(img1,127,255,cv2.THRESH_BINARY)
 
@@ -20,7 +20,7 @@ plt.title('Popocatépetl')
 plt.axis('off')
 
 plt.subplot(1,3,2)
-plt.imshow(img3, 'gray')
+plt.imshow(img2, 'gray')
 plt.title('MANUAL')
 plt.axis('off')
 
